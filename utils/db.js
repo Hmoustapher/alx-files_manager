a8ed7b6b78b12e97ff371c3a25de508a5fb49de6
hmoustapher <hmoustapher@gmail.com>
2024-12-11 05:25:55 -0800

    File manager project

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+    this.url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(this.url, { useUnifiedTopology: true });
+    this.client.connect();
+    this.db = this.client.db(database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
+
